66f46491a443fbb6d6ab4732ac442ff0cea4c49d
umikami <ggoku02@gmail.com>
2022-02-18 19:12:54 -0500

    formatted document and fixed some issues || project finished

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -3,48 +3,61 @@ import { useState } from 'react';
 
 const TrafficLight = () => {
 	const [ color, setColor ] = useState(null);
-    const [clickNum, setClickNum] = useState(0)
+	const [ clickNum, setClickNum ] = useState(0);
 
 	const cycleColor = (run) => {
-        if(run){
-            let counter = 0;
-            setClickNum((prevState) => {return prevState+1})
-    
-            setInterval(() => {
-                counter++;
-                if (counter % 3 === 1) {
-                    setColor('red');
-                } else if (counter % 3 === 2) {
-                    setColor('yellow');
-                } else if (counter % 3 === 0) {
-                    setColor('green');
-                }
-            }, 1000);
-        }
+		if (run) {
+			let counter = 0;
+			setClickNum((prevState) => {
+				return prevState + 1;
+			});
+
+			setInterval(() => {
+				counter++;
+				if (counter % 3 === 1) {
+					setColor('red');
+				} else if (counter % 3 === 2) {
+					setColor('yellow');
+				} else if (counter % 3 === 0) {
+					setColor('green');
+				}
+			}, 1000);
+		}
 	};
+
+	const lightsArr = [
+		<div
+			className={`red-light light ${color === 'red' ? 'selected-red' : null}`}
+			onClick={() => setColor('red')}
+            key={1}
+		/>,
+		<div
+			className={`yellow-light light ${color === 'yellow' ? 'selected-yellow' : null}`}
+			onClick={() => setColor('yellow')}
+            key={2}
+		/>,
+		<div
+			className={`green-light light ${color === 'green' ? 'selected-green' : null}`}
+			onClick={() => setColor('green')}
+            key={3}
+		/>
+	];
+
 
 	return (
 		<div className="main-container">
 			<div className="hanging-cord" />
-			<div className="light-container">
-				<div
-					className={`red-light light ${color === 'red' ? 'selected-red' : null}`}
-					onClick={() => setColor('red')}
-				/>
-				<div
-					className={`yellow-light light ${color === 'yellow' ? 'selected-yellow' : null}`}
-					onClick={() => setColor('yellow')}
-				/>
-				<div
-					className={`green-light light ${color === 'green' ? 'selected-green' : null}`}
-					onClick={() => setColor('green')}
-				/>
-			</div>
-			<button className="btn" onClick={() => {
-                clickNum > 1 ? cycleColor(false) : cycleColor(true)
-            }}>
-				Cycle
-			</button>
+			<div className="light-container">{lightsArr}</div>
+			<section className="controls">
+				<button
+					className="btn"
+					onClick={() => {
+						clickNum > 1 ? cycleColor(false) : cycleColor(true);
+					}}
+				>
+					Cycle
+				</button>
+			</section>
 		</div>
 	);
 };
